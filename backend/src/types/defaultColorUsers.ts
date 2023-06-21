4734b83500a853208a8389fa2f3332985cecb342
Alexander Ramian <alexander.ramian@htw-dresden.de>
2023-06-21 18:00:45 +0200

    refactor: Use orange instead of blue for in tactonGraph

--- backend/src/types/defaultColorUsers.ts
+++ backend/src/types/defaultColorUsers.ts
@@ -1,2 +1,2 @@
 //valid user colors
-export const defaultColorUsers =["#1b00c2","#7c00c2","#c20046","#008ac2", "#e7e41c", "#940900", "#940070", "#946e00", "#3A4C82"] 
+export const defaultColorUsers =["#ec660c","#8baddb","#c20046","#008ac2", "#e7e41c", "#940900", "#940070", "#946e00", "#3A4C82"] 
