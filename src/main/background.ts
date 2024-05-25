e19f6d2f65beec79929049ffdd66cb63f7e1624f
Richard Böhme <richard.boehme1999@gmail.com>
2024-05-25 15:02:15 +0200

    fix: disallow window resizes and set resolution to 1600x900
    Fixes #29

--- src/main/background.ts
+++ src/main/background.ts
@@ -23,9 +23,9 @@ protocol.registerSchemesAsPrivileged([
 async function createWindow() {
   // Create the browser window.
   const win = new BrowserWindow({
-    width: 1920,
-    height: 1080,
-    resizable: true,
+    width: 1600,
+    height: 900,
+    resizable: false,
     autoHideMenuBar: true,
     title: "Collaborative Tacton Generator",
     backgroundColor: "#121212",
