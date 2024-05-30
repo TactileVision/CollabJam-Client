cdcfc97cd92308ae7da113ac96ab174c5d3b7245
Christopher.Chlebowski <s80456@informatik.htw-dresden.de>
2024-05-30 18:39:13 +0200

    feat: added Snackbar-Component for displaying messages
    - if text-prop changes, the component automatically shows snackbar

---
+++ src/renderer/components/Snackbar.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="text-center">
+    <v-snackbar v-model="snackbar" timeout="2000" class="d-flex">
+      {{ text }}
+      <template v-slot:actions>
+        <v-btn
+            color="primary"
+            variant="text"
+            @click="snackbar = false"
+            icon="mdi-close"
+        >
+        </v-btn>
+      </template>
+    </v-snackbar>    
+  </div>
+</template>
+
+<style scoped lang="scss">
+
+</style>
+
+<script lang="ts">
+import {defineComponent} from "vue";
+export default defineComponent({
+  name: "SnackBar",
+  props: {
+    text: {
+      type: String,
+      required: true
+    }
+  },
+  data() {
+    return {
+      snackbar: false
+    }
+  },
+  watch: {
+    text(newValue) {
+      if (newValue) {
+        this.snackbar = true;
+      }
+    }
+  }
+});
+</script>
