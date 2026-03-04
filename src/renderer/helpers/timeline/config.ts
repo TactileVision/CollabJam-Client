63ea970f3e5d5f48a38aeb98961e5c4eaa5f32a1
Christopher Chlebowski <christopher.chlebowski@htw-dresden.de>
2026-03-04 13:12:43 +0100

    feat: increase baseTrackDurationt to 10 secs

--- src/renderer/helpers/timeline/config.ts
+++ src/renderer/helpers/timeline/config.ts
@@ -12,7 +12,7 @@ const config = {
   horizontalScrollThreshold: 100,
   verticalScrollSpeed: 0.1,
   verticalScrollThreshold: 100,
-  baseTrackDurationMs: 5000,
+  baseTrackDurationMs: 10000,
   // slider
   sliderHeight: 28,
   sliderMinWidth: 5,
